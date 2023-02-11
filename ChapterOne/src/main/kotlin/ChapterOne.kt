66891625adc700cd4929a24440411d9cbfe6b59c
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2023-02-11 12:03:20 -0500

    Extracting the amout calculation into its own method

--- ChapterOne/src/main/kotlin/ChapterOne.kt
+++ ChapterOne/src/main/kotlin/ChapterOne.kt
@@ -15,24 +15,7 @@ class Customer(private val name: String, private val rentals: Array<Rental>) {
         var frequentRenterPoints: Int = 0
         var result = "Rental record for $name \n"
         rentals.map {
-            var thisAmount: Double = 0.0
-            when(it.movie.movieType) {
-                MoviesType.CHILDREN -> {
-                    thisAmount += 2
-                    if (it.daysRented > 2) {
-                        thisAmount += (it.daysRented - 2) * 1.5
-                    }
-                }
-                MoviesType.NEW_RELEASE -> {
-                    thisAmount += it.daysRented  * 1.5
-                }
-                MoviesType.REGULAR -> {
-                    thisAmount += 1.5
-                    if (it.daysRented > 3) {
-                        thisAmount += (it.daysRented - 3) * 1.5
-                    }
-                }
-            }
+            var thisAmount: Double = amountFor(it)
             //add frequent renter points
             frequentRenterPoints ++
             //add bonus for a two day new release rental
@@ -48,6 +31,28 @@ class Customer(private val name: String, private val rentals: Array<Rental>) {
         result += "Amount owed is $totalAmount \n"
         result += "You earned $frequentRenterPoints frequent renter points"
         return result
+    }
+
+    private fun amountFor(it: Rental): Double {
+        var thisAmount = 0.0
+        when (it.movie.movieType) {
+            MoviesType.CHILDREN -> {
+                thisAmount += 2
+                if (it.daysRented > 2) {
+                    thisAmount += (it.daysRented - 2) * 1.5
+                }
+            }
+            MoviesType.NEW_RELEASE -> {
+                thisAmount += it.daysRented * 1.5
+            }
+            MoviesType.REGULAR -> {
+                thisAmount += 1.5
+                if (it.daysRented > 3) {
+                    thisAmount += (it.daysRented - 3) * 1.5
+                }
+            }
+        }
+        return thisAmount
     }
 }
 
